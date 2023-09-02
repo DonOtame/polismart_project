9351b5bbd9250d5762fd4d0d083ce295b606c899
Danny Cabrera <78981957+DonOtame@users.noreply.github.com>
2023-09-02 12:28:08 -0500

    Color select

--- lib/screens/form_materia.dart
+++ lib/screens/form_materia.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class IngresoMateriaScreen extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class _IngresoMateriaScreenState extends State<IngresoMateriaScreen> {
   TimeOfDay selectedStartTime = TimeOfDay.now();
   TimeOfDay selectedEndTime = TimeOfDay.now();
   bool isFirstHorario = true;
+  Color selectedColor = Colors.blue; // Color por defecto
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +47,17 @@ class _IngresoMateriaScreenState extends State<IngresoMateriaScreen> {
               Padding(
                 padding: const EdgeInsets.only(top: 16.0),
                 child: Text(
+                  'Color',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              _buildColorPicker(),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Text(
                   'Horario',
                   style: TextStyle(
                     fontSize: 18,
@@ -68,6 +81,86 @@ class _IngresoMateriaScreenState extends State<IngresoMateriaScreen> {
         },
         foregroundColor: Color(0xFFD9CE9A), // Cambia el color del icono aquí
         child: Icon(Icons.save),
+      ),
+    );
+  }
+
+  Widget _buildColorPicker() {
+    return TextButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              content: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ColorPicker(
+                      pickerColor: selectedColor,
+                      onColorChanged: (color) {
+                        setState(() {
+                          selectedColor = color;
+                        });
+                      },
+                      colorPickerWidth: 300.0,
+                      pickerAreaHeightPercent: 0.7,
+                      enableAlpha: true,
+                      displayThumbColor: true,
+                      paletteType: PaletteType.hsv,
+                    ),
+                    SizedBox(
+                        height:
+                            16.0), // Espaciado entre el selector de color y el botón
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10.0), // Ajusta el radio de borde
+                        ),
+                      ),
+                      child: Text(
+                        'Aceptar',
+                        style: TextStyle(
+                          color: Colors
+                              .white, // Cambia el color del texto del botón
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+      style: TextButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0), // Ajusta el radio de borde
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: selectedColor,
+              borderRadius:
+                  BorderRadius.circular(10.0), // Ajusta el radio de borde
+            ),
+          ),
+          SizedBox(width: 8.0), // Espaciado entre el icono y el texto
+          Text(
+            'Seleccionar Color',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
