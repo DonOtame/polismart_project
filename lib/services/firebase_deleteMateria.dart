8598aadc7126badd5787d2d9edaaa04fca0fe02d
Danny Cabrera <78981957+DonOtame@users.noreply.github.com>
2023-09-10 12:15:15 -0500

    Update firebase_deleteMateria.dart

--- lib/services/firebase_deleteMateria.dart
+++ lib/services/firebase_deleteMateria.dart
@@ -53,3 +53,67 @@ Future<bool> confirmDeleteMateria(
 
   return confirmDelete == true;
 }
+
+Future<void> eliminarMaterial(
+    BuildContext context, String nombreMateria, String url) async {
+  final confirmacion = await showDialog<bool>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Confirmar Eliminación'),
+        content:
+            const Text('¿Estás seguro de que quieres eliminar este documento?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text('Sí'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('No'),
+          ),
+        ],
+      );
+    },
+  );
+
+  if (confirmacion != null && confirmacion) {
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('materias')
+          .doc(nombreMateria)
+          .collection('documentos')
+          .where('url', isEqualTo: url)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        final documentId = querySnapshot.docs.first.id;
+        await FirebaseFirestore.instance
+            .collection('materias')
+            .doc(nombreMateria)
+            .collection('documentos')
+            .doc(documentId)
+            .delete();
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('El material se ha eliminado con éxito.'),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content:
+                Text('No se encontró el documento con el URL proporcionado.'),
+          ),
+        );
+      }
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error al eliminar el material: $error'),
+        ),
+      );
+    }
+  }
+}
